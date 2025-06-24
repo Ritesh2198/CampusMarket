1ceff68a17c95d4cb6f726b4f4e624678f618dea
Ritesh2198 <ritesh54329@gmail.com>
2025-06-24 14:49:00 +0530

    Toast

--- src/pages/Signup.jsx
+++ src/pages/Signup.jsx
@@ -1,6 +1,7 @@
 import React, { useState } from "react";
 import axios from "axios";
 import { Link, useNavigate } from "react-router-dom";
+import {toast} from "react-hot-toast"
 
 const Signup = () => {
     const navigate = useNavigate();
