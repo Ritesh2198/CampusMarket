12f602ddc83defb4c6333fa75a658cd126225314
Ritesh2198 <ritesh54329@gmail.com>
2025-06-25 12:50:17 +0530

    Fixed products display

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -36,7 +36,8 @@ const Home = () => {
     
 
     return (
-        <div className="container mx-auto p-4 grid-cols-1 md:grid-cols-2 lg:grid-cols-3">
+        //<div className="container mx-auto p-4 grid-cols-1 md:grid-cols-2 lg:grid-cols-3">
+        <div className="container mx-auto p-4 gap-4 grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3">
             <h1 className="text-2xl font-bold mb-4">Latest Products</h1>
 
             {loading ? (
